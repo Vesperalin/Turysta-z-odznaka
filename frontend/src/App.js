1a6329c33cb3d853c21e19085f05169229d879ad
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-12 16:14:59 +0100

    File structure for frontend and basic style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,9 @@
 import React from "react";
-import "./App.css";
-import { useState, useEffect } from "react";
 
-function App() {
-  const [labeledPoints, setLabeledPoints] = useState([]);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/labeled-points", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((data) => setLabeledPoints(data))
-      .catch((err) => console.log(err));
-  }, []);
-
+function App() {  
   return (
-    <div className="App">
-      <h1>Turysta z odznaką</h1>
-      {labeledPoints.map((labeledPoint) => {
-        return (
-          <div key={labeledPoint.id_punktu}>
-            <p>{labeledPoint.nazwa} {labeledPoint.wysokosc ? labeledPoint.wysokosc + ' m n.p.m.': ''} </p>
-          </div>
-        );
-      })}
+    <div>
+      <p>Hello</p>
     </div>
   );
 }
