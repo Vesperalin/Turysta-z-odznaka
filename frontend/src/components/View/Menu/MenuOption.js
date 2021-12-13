11c8ca6a81552ec4aa1885e4a1acfeb2e0bd6180
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-13 12:54:12 +0100

    Implement searchbar (topbar) component and add new line at the end of files

--- frontend/src/components/View/Menu/MenuOption.js
+++ frontend/src/components/View/Menu/MenuOption.js
@@ -3,12 +3,12 @@ import React from "react";
 import styles from "./MenuOption.module.css";
 
 const MenuOption = props => {
-    return (
-        <a href="https://www.google.com/" className={styles.link}>
-            <img src={props.imagePath} alt="option-icon"/>
-            <p>{props.children}</p>
-        </a>
-    );
+  return (
+    <a href="https://www.google.com/" className={styles.link}>
+      <img src={props.imagePath} alt="option-icon" />
+      <p>{props.children}</p>
+    </a>
+  );
 };
 
 export default MenuOption;
