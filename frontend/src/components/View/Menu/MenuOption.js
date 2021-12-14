dd09c49d81fbbede40df12e58a5998992aa4430c
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-14 11:54:25 +0100

    Implement links to routes on main page

--- frontend/src/components/View/Menu/MenuOption.js
+++ frontend/src/components/View/Menu/MenuOption.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import styles from "./MenuOption.module.css";
 
 const MenuOption = props => {
   return (
-    <a href="https://www.google.com/" className={styles.link}>
+    <NavLink to={props.path} className={(navData) => navData.isActive ? `${styles.link} ${styles.linkActive}` : styles.link}>
       <img src={props.imagePath} alt="option-icon" />
       <p>{props.children}</p>
-    </a>
+    </NavLink >
   );
 };
 
