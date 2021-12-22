dfeba2e7cb68be522bf2ef1b43dd90f2fd29e380
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-22 19:09:43 +0100

    Add axios to fetch data about labeled points

---
+++ frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchForm/SearchForm.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const baseURL = "http://127.0.0.1:5000/labeled-points";
+
+const SearchForm = () => {
+  const [labeledPoints, setLabeledPoints] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    axios.get(baseURL)
+      .then(response => {
+        setLabeledPoints(response.data);
+      })
+      .catch(error => setErrorMessage(error.response.data))
+  }, [])
+
+  return (
+    <div>
+      <p>Hi</p>
+      <p>{errorMessage}</p>
+      {labeledPoints.map((labeledPoint) => {
+        return (
+          <div key={labeledPoint.id}>
+            <p>{labeledPoint.name} {labeledPoint.height ? labeledPoint.height + ' m n.p.m.' : ''} </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SearchForm;
--- frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchLabeledPoint.js
+++ frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchLabeledPoint.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+import SearchForm from "./SearchForm/SearchForm";
+import styles from "./SearchLabeledPoint.module.css";
+
 
 const SearchLabeledPoint = () => {
+  const [formIsShown, setFormIsShown] = useState(true);
+  //const [tableIsShown, setTableIsShown] = useState(false);
+  //const [infoIsShown, setInfoIsShown] = useState(false);
+  //muszę zrobić podawanie info z dołu do rodzica jak user wybierze jaką nazwe chce wpisać - może przez funkcje wywoływaną z dołu przez props
+  //conditional rendering wyniku wyszukiwania
+
   return (
-    <div>
-      <h2>Wyszuka punkt</h2>
+    <div className={styles.wrapper}>
+      <h2>Szukaj punktu opisanego</h2>
+      {formIsShown && <SearchForm />}
     </div>
   );
 };
