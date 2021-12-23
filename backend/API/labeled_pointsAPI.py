75f09b29041ab7de6bb72baa777e2c192512110a
justyna-maluszynska <maluszynskaj@gmail.com>
2021-12-23 19:42:24 +0100

    Change method of returning exceptions

--- backend/API/labeled_pointsAPI.py
+++ backend/API/labeled_pointsAPI.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask.json import jsonify
 from flask.blueprints import Blueprint
 from models.labeled_point import Labeled_point
@@ -21,31 +21,19 @@ def get_labeled_points_like(like):
     return labeled_point_schema.jsonify(Labeled_point.query.filter(Labeled_point.name.like("%{}%".format(like))), many=True), 200
 
 
-"""
-Needs to be checked (based on mockups):
-- if requested point exist (DONE)
-"""
-
-
 @router.route('/<int:id>', methods=['GET'])
 def get_labeled_point(id):
     labeled_point = Labeled_point.query.get(id)
 
     if not labeled_point:
-        return {'message': 'Labeled point not available'}, 404
+        return Response("{'message':'Punkt o wybranej nazwie nie istnieje!'}", status=404, mimetype='application/json')
 
     return labeled_point_schema.jsonify(labeled_point), 200
 
 
 """
-Needs to be checked (based on mockups):
-- if name is unique (done)
-- if height is correct (>=0 or None) (DONE)
-
-#TODO Nazwa to lowerCase z wyjątkiem pierwszego znaku (Wielka litera)
-
 Required JSON:
-"name":
+"name": "nameValue"
 "height": (value or null)
 """
 
@@ -55,26 +43,20 @@ def add_labeled_point():
     labeled_point_dictionary = request.get_json()
     capitalize(labeled_point_dictionary)
 
-    if(is_name_unique(labeled_point_dictionary['name'])):
-        if is_height_correct(labeled_point_dictionary['height']):
+    if __is_name_unique(labeled_point_dictionary['name']):
+        if __is_height_correct(labeled_point_dictionary['height']):
             own_point = labeled_point_schema.load(labeled_point_dictionary)
             own_point.save()
             return labeled_point_schema.jsonify(own_point), 200
         else:
-            return {'message': 'Incorrect height value'}, 404
+            return Response("{'message':'Niepoprawna wysokość!'}", status=404, mimetype='application/json')
     else:
-        return {'message': 'Name of point is not unique'}, 404
+        return Response("{'message':'Punkt o wybranej nazwie już istnieje!'}", status=404, mimetype='application/json')
 
 
 """
-Needs to be checked (based on mockups):
-- if requested point exist (done)
-- if requested point is in usage (done)
-- if new name is unique (done)
-- if new height is correct (>=0 or None)
-
 Required JSON:
-"name":
+"name": "nameValue"
 "height": (value or null)
 """
 
@@ -84,17 +66,17 @@ def update_labeled_point(id):
     existing_labeled_point = Labeled_point.query.get(id)
 
     if not existing_labeled_point:
-        return {'message': 'Requested labeled point not available'}, 404
+        return Response("{'message':'Punkt o wybranej nazwie nie istnieje!'}", status=404, mimetype='application/json')
 
-    if is_in_usage(existing_labeled_point):
-        return {'message': 'Requested labeled point is already in usage and cannot be modified'}, 404
+    if __is_in_usage(existing_labeled_point):
+        return Response("{'message':'Punkt jest już używany w odcinkach. Nie można go edytować!'}", status=404, mimetype='application/json')
 
     new_point_data = request.get_json()
 
-    message, code = verify_new_data(existing_labeled_point, new_point_data)
+    message, code = __verify_new_data(existing_labeled_point, new_point_data)
 
     if code == 404:
-        return message, code
+        return Response("{'message':'{}}'}".format(message), status=code, mimetype='application/json')
 
     labeled_point = labeled_point_schema.load(
         new_point_data, instance=existing_labeled_point, partial=True)
@@ -104,9 +86,7 @@ def update_labeled_point(id):
 
 
 """
-Needs to be checked (based on mockups):
-- if requested point exist (done)
-- if requested point is in usage (done)
+
 """
 
 
@@ -115,16 +95,16 @@ def delete_labeled_point(id):
     labeled_point = Labeled_point.query.get(id)
 
     if not labeled_point:
-        return {'message': 'Requested labeled point not available'}, 404
+        return Response("{'message':'Punkt o wybranej nazwie nie istnieje!'}", status=404, mimetype='application/json')
 
-    if is_in_usage(labeled_point):
-        return {'message': 'Requested labeled point is in usage and cannot be deleted'}, 404
+    if __is_in_usage(labeled_point):
+        return Response("{'message':'Punkt jest już używany w odcinkach. Nie można go usunąć. Zamiast tego przenieś punkt do kategorii zlikwidowane.'}", status=404, mimetype='application/json')
 
     labeled_point.remove()
-    return {'message': 'Labeled point successfully deleted'}, 200
+    return {'message': 'Punkt został pomyślnie usunięty'}, 200
 
 
-def is_name_unique(name: str):
+def __is_name_unique(name: str):
     labeled_point = Labeled_point.query.filter(
         Labeled_point.name.like(name)).first()
 
@@ -134,7 +114,7 @@ def is_name_unique(name: str):
     return False
 
 
-def is_in_usage(labeled_point: Labeled_point):
+def __is_in_usage(labeled_point: Labeled_point):
     if len(labeled_point.start_of_own_segments) > 0:
         return True
     if len(labeled_point.end_of_own_segments) > 0:
@@ -147,19 +127,19 @@ def is_in_usage(labeled_point: Labeled_point):
     return False
 
 
-def verify_new_data(existing_point: Labeled_point, data):
+def __verify_new_data(existing_point: Labeled_point, data):
     is_new_name = existing_point.name != data['name']
     if is_new_name:
-        if not is_name_unique(data['name']):
+        if not __is_name_unique(data['name']):
             return {'message': 'New name of point must be unique'}, 404
 
     is_new_height = existing_point.height != data['height']
     if is_new_height:
-        if not is_height_correct(data['height']):
+        if not __is_height_correct(data['height']):
             return {'message': 'Incorrect new height of point'}, 404
 
     return None, 200
 
 
-def is_height_correct(height):
+def __is_height_correct(height):
     return True if height is None else (height >= 0 and isinstance(height, int))
