110e489f0ae9e956ff47c8b86f61d599616237f4
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-23 09:59:57 +0100

    Comment error handling - endpoints not working properly on backend

--- frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchForm/SearchForm.js
+++ frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchForm/SearchForm.js
@@ -5,20 +5,21 @@ const baseURL = "http://127.0.0.1:5000/labeled-points";
 
 const SearchForm = () => {
   const [labeledPoints, setLabeledPoints] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
+  //const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     axios.get(baseURL)
       .then(response => {
         setLabeledPoints(response.data);
       })
-      .catch(error => setErrorMessage(error.response.data))
+      //.catch(error => setErrorMessage(error.response.data))
+      .catch(error => console.log(error.response.data))
   }, [])
 
   return (
     <div>
       <p>Hi</p>
-      <p>{errorMessage}</p>
+      {/*<p>{errorMessage}</p>*/}
       {labeledPoints.map((labeledPoint) => {
         return (
           <div key={labeledPoint.id}>
