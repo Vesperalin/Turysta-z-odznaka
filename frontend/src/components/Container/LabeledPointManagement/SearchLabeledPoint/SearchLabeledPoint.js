f06b216e39bfcad5be62e7cf7689c9bd56f9deab
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-23 15:18:13 +0100

    Style the search form

--- frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchLabeledPoint.js
+++ frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchLabeledPoint.js
@@ -8,6 +8,7 @@ const SearchLabeledPoint = () => {
   const [formIsShown, setFormIsShown] = useState(true);
   //const [tableIsShown, setTableIsShown] = useState(false);
   //const [infoIsShown, setInfoIsShown] = useState(false);
+  
   //muszę zrobić podawanie info z dołu do rodzica jak user wybierze jaką nazwe chce wpisać - może przez funkcje wywoływaną z dołu przez props
   //conditional rendering wyniku wyszukiwania
 
