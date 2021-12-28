ebaf7559681a0d4b062976eab02c158c2b318302
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-28 11:13:53 +0100

    Improve error handling in delete for labeled point

--- frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchLabeledPoint.js
+++ frontend/src/components/Container/LabeledPointManagement/SearchLabeledPoint/SearchLabeledPoint.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 import SearchForm from "../SearchForm";
 import styles from "./SearchLabeledPoint.module.css";
 import LabeledPointsSearchResultTable from "../../../View/LabeledPointsSearchResultTable/LabeledPointsSearchResultTable";
-import { useNavigate } from "react-router-dom";
 
 const likeBaseURL = "http://127.0.0.1:5000/labeled-points/like";
 const labeledPointsBaseURL = "http://127.0.0.1:5000/labeled-points";
