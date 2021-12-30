73cde991f8775fb7659b34816ffebe06d555b85d
justyna-maluszynska <maluszynskaj@gmail.com>
2021-12-30 12:34:53 +0100

    Add GET endpoint for labeled-point in tour_creationAPI

--- backend/API/tour_creationAPI.py
+++ backend/API/tour_creationAPI.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.blueprints import Blueprint
 from services.tour_creation_services import get_labeled_segments
+from services.labeled_point_services import get_labeled_point
 
 router = Blueprint('tour-creation', __name__)
 
@@ -8,3 +9,8 @@ router = Blueprint('tour-creation', __name__)
 @router.route('/labeled-segments', methods=['GET'])
 def get_segments():
     return get_labeled_segments()
+
+
+@router.route('/labeled-points/<int:id>', methods=['GET'])
+def get_point(id):
+    return get_labeled_point(id)
