432a8e0d6e02e518354232e56cb6147b6666279c
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-31 12:19:15 +0100

    Add data fetching from API to TourCreation component

--- frontend/src/components/Container/TourCreation/TourCreation.js
+++ frontend/src/components/Container/TourCreation/TourCreation.js
@@ -1,22 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./TourCreation.module.css";
+
+const labeledPointsBaseURL = "http://127.0.0.1:5000/labeled-points";
+const labeledSegmentsBaseURL = "http://127.0.0.1:5000/tour-creation/labeled-segments";
 
 // #TODO add media queries for points presenter
 
 const TourCreation = () => {
   const [isTourCreationFormShown, setIsTourCreationFormShown] = useState(true);
   const [isTourNameFormShown, setIsTourNameFormShown] = useState(false);
-  const [points, setPoints] = useState(0);
-  //const [labeledSegments, setLabeledSegments] = useState([]);
-  const [labeledPoints, setLabeledPints] = useState([]);
+  const [points, setPoints] = useState(0); // #TODO - dopisać do tego useEffect, że jak się zmienią chosenSegments to punktu się będą obliczać
+  const [labeledSegments, setLabeledSegments] = useState([]);
+  const [labeledPoints, setLabeledPoints] = useState([]);
   //const [chosenSegments, setChosenSegments] = useState([]);
   //const [startingPoint, setStartingPoint] = useState("");
+  const navigate = useNavigate();
 
-  
+  useEffect(() => {
+    axios.get(labeledPointsBaseURL)
+      .then(response => {
+        setLabeledPoints(response.data);
+      })
+      .catch(error => {
+        if (error.response.status === 503) {
+          navigate('/503');
+        } else {
+          navigate('/error');
+        }
+      });
 
+      axios.get(labeledSegmentsBaseURL)
+      .then(response => {
+        setLabeledSegments(response.data);
+      })
+      .catch(error => {
+        if (error.response.status === 503) {
+          navigate('/503');
+        } else {
+          navigate('/error');
+        }
+      });
+  }, [navigate]);
 
   return (
     <div className={styles.wrapper}>
