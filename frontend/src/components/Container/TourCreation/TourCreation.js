ee963b3ea2e3f3b9962cd0b3ec6b4de3b226c7e5
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2021-12-31 12:10:04 +0100

    Create basic TourCreation component

--- frontend/src/components/Container/TourCreation/TourCreation.js
+++ frontend/src/components/Container/TourCreation/TourCreation.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./TourCreation.module.css";
+
+// #TODO add media queries for points presenter
 
 const TourCreation = () => {
+  const [isTourCreationFormShown, setIsTourCreationFormShown] = useState(true);
+  const [isTourNameFormShown, setIsTourNameFormShown] = useState(false);
+  const [points, setPoints] = useState(0);
+  //const [labeledSegments, setLabeledSegments] = useState([]);
+  const [labeledPoints, setLabeledPints] = useState([]);
+  //const [chosenSegments, setChosenSegments] = useState([]);
+  //const [startingPoint, setStartingPoint] = useState("");
+
+  
+
+
   return (
-    <div>
-      <h2>Tworzenie trasy</h2>
+    <div className={styles.wrapper}>
+      {(isTourCreationFormShown && !isTourNameFormShown) &&
+        <>
+          <>
+            <h3>Stwórz trasę poprzez podanie punktów tworzących odcinki</h3>
+            <div className={styles.temp}></div>
+          </>
+          <div className={styles.pointsPresenter}>
+            <p>Punkty do GOT</p>
+            <p>{points}</p>
+          </div>
+        </>
+      }
+      {(!isTourCreationFormShown && isTourNameFormShown) &&
+        <>
+          <p>Tu będzie nadawanie trasie nazwy</p>
+        </>
+      }
+      {(!isTourCreationFormShown && !isTourNameFormShown) &&
+        <>
+          <p>Tu będzie komunikat zwrotny</p>
+        </>
+      }
     </div>
   );
 };
